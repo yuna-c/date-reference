26ecd748e501e6ae3952dd9b341d863bcd7f438a
yuna-c <yuna744@gmail.com>
2023-11-14 12:38:52 +0900

    1114 특정시차의 시간대 구하는 방법;

--- js/main.js
+++ js/main.js
@@ -13,3 +13,17 @@ console.dir(now);
 console.dir(year);
 console.log(monthName.length, monthName[10]); //월의 값이 아닌 순서값 [0: 1월]
 console.log(time); // 표준시가 지정된 시점으로 부터의 밀리세컨드 값이 반환
+
+const worldHour = now.getUTCHours();
+
+//특정 나라 표준시 구하는 방법
+//우리나라 시간대를 밀리세컨드로 반환후 - 밀리세컨드 = 시차
+
+//만약 8시간 빠른 독일 시간 구하고 싶으면?
+const timeGerman = time - 1000 * 60 * 60 * 8; //1000 1초
+
+//해당 밀리세컨드 값을 new Date()의 인수로 전달
+//해당 밀리세컨드 시간 값에 시간 인스턴스 값이 수정되서 반환됨
+const nowGerman = new Date(timeGerman);
+console.log('한국시', now);
+console.log('독일시', nowGerman);
